504484f53830e0c6e1cad17ecb9b989ae2895be7
Persephone <its.persephone.xo@gmail.com>
2024-04-13 12:55:39 -0400

    added to the css and still need to work on the js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,25 @@ const accordion=document.getElementsByClassName('accordion');
 
 for (let i=0;i<accordion.length;i++){
     accordion[i].addEventListener('click',()=>{
+        //toggle the buttons that control the panel
+        accordion[i].classList.toggle('open');
+        accordion[i].nextElementSibling.classList.toggle('open');
+
+        //hide all panels
+        let allPanels=document.getElementsByClassName('panel');
+        for(let each of allPanels){
+            each.style.display='none';
+        }
+
+        //toggle between hiding and showing the panel
+        let panel=accordion[i].parentNode.parentNode.querySelector('.panel');
+        panel.classList.toggle('show');
+
+        if(panel.style.display=='none' && panel.classList.contains('show')){
+            panel.style.display='block';
+        }else{
+            panel.style.display='none';
+        }
 
     });
 }
